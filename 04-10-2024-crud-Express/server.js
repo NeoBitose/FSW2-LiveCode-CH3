23c829cdfa8605c45b8a3f2ce3797ffecae7d95d
NeoBitose <ramadhanalif430@gmail.com>
2024-10-06 19:35:27 +0700

    feat: add livecode 04-10-2024

--- 04-10-2024-crud-Express/server.js
+++ 04-10-2024-crud-Express/server.js
@@ -65,6 +65,15 @@ app.get('/api/v1/cars/:id', (req, res) => {
     const car = cars.find((i) => i.id === req.params.id); 
     console.log(car)
 
+    if (!car) {
+        res.status(404).json({
+            "status": "Failed",
+            "message": "Get data not successfully",
+            "isSuccess": false,
+            "data": null
+        })
+    }
+
     res.status(200).json({
         "status": "success",
         "message": "Get data successfully",
@@ -74,19 +83,72 @@ app.get('/api/v1/cars/:id', (req, res) => {
 })
 
 app.patch('/api/v1/cars/:id', (req, res) => {
+    const id = req.params.id;
     // update into
     console.log(req.body)
+    const {name, year, type} = req.body;
 
-    // const car = cars.find((i) => i.id === req.params.id); 
-    // console.log(car)
+    const car = cars.find((i) => i.id === req.params.id); 
+    if (!car) {
+        res.status(500).json({
+            "status": "failed",
+            "message": "Get data not successfully",
+            "isSuccess": false,
+            "data": null
+        })
+    }
+    console.log(car)
 
-    res.status(200).json({
-        "status": "success",
-        "message": "Get data successfully",
-        "isSuccess": true,
-        // "data": {car}
+    const carIndex = cars.findIndex((car) => car.id === id);
+    console.log(carIndex);
+
+    cars[carIndex] = {...cars[carIndex], ...req.body}
+    console.log(cars)
+
+    fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
+        const newCar = cars.find((i) => i.id === req.params.id); 
+        res.status(201).json({
+            "status": "success",
+            "message": "Edit data successfully",
+            "isSuccess": true,
+            "data": {
+                newCar
+            }
+        })
     })
 })
+
+app.delete('/api/v1/cars/:id', (req, res) => {
+    const id = req.params.id;
+    // select where
+    console.log(req.params)
+    const car = cars.find((i) => i.id === id); 
+    console.log(car)
+
+    if (!car) {
+        res.status(404).json({
+            "status": "Failed",
+            "message": "Get data not successfully",
+            "isSuccess": false,
+            "data": null
+        })
+    }
+
+    const carIndex = cars.findIndex((car) => car.id === id);
+    cars.splice(carIndex,1)
+
+    fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
+        const newCar = cars.find((i) => i.id === req.params.id); 
+        res.status(201).json({
+            "status": "success",
+            "message": "Delete data successfully",
+            "isSuccess": true,
+            "data": null
+        })
+    })
+})
+
+
 
 // Middleware
 // Personal Middleware
