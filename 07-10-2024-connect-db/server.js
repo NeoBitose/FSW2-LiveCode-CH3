b3d91ca23d9f7683cef4824e32db42835fbfd1c7
NeoBitose <ramadhanalif430@gmail.com>
2024-10-07 21:20:07 +0700

    feat: addconnection postgresql

--- 07-10-2024-connect-db/server.js
+++ 07-10-2024-connect-db/server.js
@@ -1,6 +1,8 @@
 const fs = require("fs")
 const express = require("express")
 
+const {product} = require('./models') 
+
 const app = express()
 
 app.use(express.json())
@@ -14,22 +16,17 @@ app.get('/', (req, res) => {
     })
 })
 
-const cars = JSON.parse(
-    fs.readFileSync(`${__dirname}/Data/cars.json`, "utf-8")
-)
-
-app.get('/api/v1/cars', (req, res) => {
+app.get('/api/v1/cars', async (req, res) => {
     // Select from
     try {
-        const cars = JSON.parse(
-            fs.readFileSync(`${__dirname}/Data/cars.json`)
-        )
+        const dataProduct = await product.findAll();
+        
         res.status(201).json({
             "status": "success",
             "message": "Get data successfully",
             "isSuccess": true,
-            "totalData": cars.length,
-            "data": {cars},
+            "totalData": dataProduct.length,
+            "data": {dataProduct},
         })
     }
     catch (error) {
@@ -42,111 +39,111 @@ app.get('/api/v1/cars', (req, res) => {
     }
 })
 
-app.post('/api/v1/cars', (req, res) => {
-    // Insert into
-    const newCar = req.body;
-    console.log(newCar);
-    cars.push(newCar);
+// app.post('/api/v1/cars', (req, res) => {
+//     // Insert into
+//     const newCar = req.body;
+//     console.log(newCar);
+//     cars.push(newCar);
 
-    fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
-        res.status(201).json({
-            "status": "success",
-            "message": "Add data successfully",
-            "isSuccess": true,
-            "totalData": cars.length,
-            "data": {newCar}
-        })
-    })
-})
+//     fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
+//         res.status(201).json({
+//             "status": "success",
+//             "message": "Add data successfully",
+//             "isSuccess": true,
+//             "totalData": cars.length,
+//             "data": {newCar}
+//         })
+//     })
+// })
 
-app.get('/api/v1/cars/:id', (req, res) => {
-    // select where
-    console.log(req.params)
-    const car = cars.find((i) => i.id === req.params.id); 
-    console.log(car)
+// app.get('/api/v1/cars/:id', (req, res) => {
+//     // select where
+//     console.log(req.params)
+//     const car = cars.find((i) => i.id === req.params.id); 
+//     console.log(car)
 
-    if (!car) {
-        res.status(404).json({
-            "status": "Failed",
-            "message": "Get data not successfully",
-            "isSuccess": false,
-            "data": null
-        })
-    }
+//     if (!car) {
+//         res.status(404).json({
+//             "status": "Failed",
+//             "message": "Get data not successfully",
+//             "isSuccess": false,
+//             "data": null
+//         })
+//     }
 
-    res.status(200).json({
-        "status": "success",
-        "message": "Get data successfully",
-        "isSuccess": true,
-        "data": {car}
-    })
-})
+//     res.status(200).json({
+//         "status": "success",
+//         "message": "Get data successfully",
+//         "isSuccess": true,
+//         "data": {car}
+//     })
+// })
 
-app.patch('/api/v1/cars/:id', (req, res) => {
-    const id = req.params.id;
-    // update into
-    console.log(req.body)
-    const {name, year, type} = req.body;
+// app.patch('/api/v1/cars/:id', (req, res) => {
+//     const id = req.params.id;
+//     // update into
+//     console.log(req.body)
+//     const {name, year, type} = req.body;
 
-    const car = cars.find((i) => i.id === req.params.id); 
-    if (!car) {
-        res.status(500).json({
-            "status": "failed",
-            "message": "Get data not successfully",
-            "isSuccess": false,
-            "data": null
-        })
-    }
-    console.log(car)
+//     const car = cars.find((i) => i.id === req.params.id); 
+//     if (!car) {
+//         res.status(500).json({
+//             "status": "failed",
+//             "message": "Get data not successfully",
+//             "isSuccess": false,
+//             "data": null
+//         })
+//     }
+//     console.log(car)
 
-    const carIndex = cars.findIndex((car) => car.id === id);
-    console.log(carIndex);
+//     const carIndex = cars.findIndex((car) => car.id === id);
+//     console.log(carIndex);
 
-    cars[carIndex] = {...cars[carIndex], ...req.body}
-    console.log(cars)
+//     cars[carIndex] = {...cars[carIndex], ...req.body}
+//     console.log(cars)
 
-    fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
-        const newCar = cars.find((i) => i.id === req.params.id); 
-        res.status(201).json({
-            "status": "success",
-            "message": "Edit data successfully",
-            "isSuccess": true,
-            "data": {
-                newCar
-            }
-        })
-    })
-})
+//     fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
+//         const newCar = cars.find((i) => i.id === req.params.id); 
+//         res.status(201).json({
+//             "status": "success",
+//             "message": "Edit data successfully",
+//             "isSuccess": true,
+//             "data": {
+//                 newCar
+//             }
+//         })
+//     })
+// })
 
-app.delete('/api/v1/cars/:id', (req, res) => {
-    const id = req.params.id;
-    // select where
-    console.log(req.params)
-    const car = cars.find((i) => i.id === id); 
-    console.log(car)
+// app.delete('/api/v1/cars/:id', (req, res) => {
+//     const id = req.params.id;
+//     // select where
+//     console.log(req.params)
+//     const car = cars.find((i) => i.id === id); 
+//     console.log(car)
 
-    if (!car) {
-        res.status(404).json({
-            "status": "Failed",
-            "message": "Get data not successfully",
-            "isSuccess": false,
-            "data": null
-        })
-    }
+//     if (!car) {
+//         res.status(404).json({
+//             "status": "Failed",
+//             "message": "Get data not successfully",
+//             "isSuccess": false,
+//             "data": null
+//         })
+//     }
 
-    const carIndex = cars.findIndex((car) => car.id === id);
-    cars.splice(carIndex,1)
+//     const carIndex = cars.findIndex((car) => car.id === id);
+//     cars.splice(carIndex,1)
 
-    fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
-        const newCar = cars.find((i) => i.id === req.params.id); 
-        res.status(201).json({
-            "status": "success",
-            "message": "Delete data successfully",
-            "isSuccess": true,
-            "data": null
-        })
-    })
-})
+//     fs.writeFile(`${__dirname}/Data/cars.json`, JSON.stringify(cars), (err) => {
+//         const newCar = cars.find((i) => i.id === req.params.id); 
+//         res.status(201).json({
+//             "status": "success",
+//             "message": "Delete data successfully",
+//             "isSuccess": true,
+//             "data": null
+//         })
+//     })
+// })
 
 // Middleware
 // Personal Middleware
